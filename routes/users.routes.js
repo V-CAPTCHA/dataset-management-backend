dbbf88da5d13de5f34308bb9fccb7c2c9a9bf14e
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-02-28 19:26:59 +0700

    Add change user information method

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const { body, validationResult } = require('express-validator');
+const { body, param, validationResult } = require('express-validator');
 
 
 //Use sequelize model
@@ -10,7 +10,7 @@ const Admin = db.admin;
 
 
 //Get all user API
-router.get('/all', async (req, res) => {
+router.get('/', async (req, res) => {
   //Find and count all key of user in database
   const {count, rows} = await User.findAndCountAll({
     attributes: [
@@ -60,5 +60,53 @@ router.get('/:user_id', async(req, res) => {
     "data": user
   });
 })
+
+
+//Change user information API
+router.patch('/:user_id', 
+  param('user_id').isInt(),
+  body('email').isEmail(),
+  body('first_name').isLength({ min: 2, max: 50 }),
+  body('last_name').isLength({ min: 2, max: 50 }),
+async(req, res) => {
+  //Validation
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const user_id = req.params.user_id;
+  const email = req.body.email
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+
+  //Check null information
+  if (!(email && first_name && last_name)) 
+    return res.status(400).json({ "message": "information is required" })
+
+  //Find user in db
+  const user = await User.findOne({ 
+    where: { user_id: user_id }
+  });
+
+  //Check exist user
+  if (!user)
+    return res.status(400).json({"message": "user does not exist "});
+
+
+  //Update user
+  await User.update(
+    {  
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+    },
+    { where: { user_id: user_id }}
+  );
+
+  res.status(200).json({
+    "message": "change user information successfully"
+  })
+}) 
 
 module.exports = router;
