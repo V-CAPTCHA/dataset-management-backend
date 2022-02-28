531adae3c9d2dee61ad171ecd124c97bb7b320c2
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-02-28 19:00:28 +0700

    Add get user and all users method

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,3 @@
-/*
-  We have 4 APIs for users route
-  1. Get a user API
-  2. Change first name and last name API
-  3. Delete account API
-  4. Change password API
-*/
-
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
@@ -14,160 +6,59 @@ const { body, validationResult } = require('express-validator');
 //Use sequelize model
 const db = require('../config/database.config');
 const User = db.user;
-const CaptchaKey = db.captcha_key;
+const Admin = db.admin;
 
 
-//Get a user API
-router.get('/', async (req, res) => {
-  const user_id = res.locals.user.user_id;
-  
-  //Find user in database
-  const user = await User.findOne({ 
+//Get all user API
+router.get('/all', async (req, res) => {
+  //Find and count all key of user in database
+  const {count, rows} = await User.findAndCountAll({
     attributes: [
+      'user_id',
       'email',
       'first_name',
       'last_name',
     ],
-    where: { user_id: user_id }
   });
 
+  //Check exist key
+  if(!count) {
+    return res.status(200).json({"message": "user does not exist"});
+  }
+
+  res.status(200).json({
+    "message": "get all users successfully",
+    "data": rows
+  })
+})
+
+
+//Get a user API
+router.get('/:user_id', async(req, res) => {
+  const user_id = req.params.user_id;
+
+  //Find a user in database
+  const user = await User.findOne({
+    attributes: [
+      'user_id',
+      'email',
+      'first_name',
+      'last_name',
+    ],
+    where: {
+      user_id: user_id
+    }
+  })
+
   //Check exist user
-  if (!user)
-    return res.status(400).json({"message": "user does not exist "});
+  if(!user) {
+    return res.status(400).json({"message": "user does not exist"});
+  }
 
   res.status(200).json({
     "message": "get user successfully",
     "data": user
   });
 })
-
-
-//Change first name and last name API
-router.post('/', 
-  body('first_name').isLength({ min: 2, max: 50 }),
-  body('last_name').isLength({ min: 2, max: 50 }),
-async (req, res) => {
-  //Validation
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const user_id = res.locals.user.user_id;
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-
-  //Check null information
-  if (!first_name || !last_name) {
-    return res.status(400).json({"message": "information is required"});
-  }
-
-  //Find user in db
-  const user = await User.findOne({ 
-    where: { user_id: user_id }
-  });
-
-  //Check exist user
-  if (!user)
-    return res.status(400).json({"message": "user does not exist "});
-
-  //Update user
-  await User.update(
-    {  
-      first_name: first_name,
-      last_name: last_name,
-    },
-    { where: { user_id: user_id }}
-  );
-
-  res.status(200).json({
-    "message": "change first name and last name successfully"
-  })
-});
-
-
-//Delete account API
-router.delete('/', async (req, res) => {
-  const user_id = res.locals.user.user_id;
-
-  //Find user in db
-  const user = await User.findOne({ 
-    where: { user_id: user_id }
-  });
-
-  //Check exist user
-  if (!user)
-    return res.status(400).json({"message": "user does not exist "});
-
-  //Delete captcha_key of user
-  await CaptchaKey.destroy({
-    where: {
-      user_id: user_id
-    }
-  });
-
-  //Delete user
-  await User.destroy({
-    where: {
-      user_id: user_id
-    }
-  });
-
-  res.status(200).json({
-    "message": "delete account successfully"
-  });
-});
-
-
-//Change password API
-router.post('/password', 
-  body('current_password').isLength({ min: 8, max: 50 }),
-  body('_new_password').isLength({ min: 8, max: 50 }),
-async (req, res) => {
-  //Validation
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const current_password = req.body.current_password;
-  const new_password = req.body.new_password;
-  const user_id = res.locals.user.user_id;
-
-  //Check null password 
-  if(!current_password || !new_password) {
-    return res.status(400).json({ "message": "password is required" })
-  }
-
-  //Find user in db
-  const user = await User.findOne({ 
-    where: { user_id: user_id }
-  });
-
-  //Check exist user
-  if(!user)
-    return res.status(400).json({"message": "user does not exist "});
-
-  //Compare password
-  const isMatch = await bcrypt.compare(current_password, user.password);
-
-  //Correct password
-  if(isMatch) {
-    //New password hashing
-    const hashNewPassword = await bcrypt.hash(new_password, 8);
-
-    //Change password
-    await User.update({ password: hashNewPassword },
-      { where : { user_id: user_id }}
-    );
-
-    res.status(200).json({"message": "change password successfully"})
-  }  
-
-  //Incorrect password
-  else {
-    res.status(401).json({"message": "incorrect current password"});
-  }
-});
 
 module.exports = router;
